04aba9c67f2cbf406f85be82769d2131c5eb9eab
Reydel Leon <reydel@surpassweb.com>
2014-02-17 18:38:34 -0500

    Deletes some non official cookbooks and substitutes them by the official ones. Adds new cookbooks

--- cookbooks/nginx/recipes/default.rb
+++ cookbooks/nginx/recipes/default.rb
@@ -1,27 +1,32 @@
 #
-# Cookbook Name:: apache
+# Cookbook Name:: nginx
 # Recipe:: default
 #
-# Copyright 2014, YOUR_COMPANY_NAME
+# Author:: AJ Christensen <aj@junglist.gen.nz>
 #
-# All rights reserved - Do Not Redistribute
+# Copyright 2008-2013, Opscode, Inc.
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#     http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
 #
 
-#Install Apache
-
-package "nginx" do
-	action :install
+case node['nginx']['install_method']
+when 'source'
+  include_recipe 'nginx::source'
+when 'package'
+  include_recipe 'nginx::package'
 end
 
-# Start apache service
-# Make sure the service starts with the server
-service "nginx" do
-	action [:start, :enable]
+service 'nginx' do
+  supports :status => true, :restart => true, :reload => true
+  action   :start
 end
-
-# Write our Home Page
-cookbook_file "/var/www/index.html" do
-	source "index.html"
-	mode "0644"
-end
-
