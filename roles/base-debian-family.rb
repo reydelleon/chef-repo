555c69c6a391f6100dc4d31473d34f1c613f5056
Reydel Leon <reydel@surpassweb.com>
2014-02-17 23:44:34 -0500

    Adds recipes in roles base-family

--- roles/base-debian-family.rb
+++ roles/base-debian-family.rb
@@ -1,6 +1,6 @@
 name "base-debian-family"
 description "Base role for Debian family servers"
-run_list "recipe[apt]", "recipe[chef-client::cron]", "recipe[python]"
+run_list "recipe[apt]", "recipe[mc]", "recipe[chef-client::cron]", "recipe[python]"
 override_attributes({
   "company" => "Daida",
 })
--- roles/base-rhel-family.rb
+++ roles/base-rhel-family.rb
@@ -1,6 +1,6 @@
 name "base-rhel-family"
 description "Base role for RHEL family servers"
-run_list "recipe[yum]", "recipe[chef-client::cron]", "recipe[python]"
+run_list "recipe[yum]", "recipe[mc]", "recipe[chef-client::cron]", "recipe[python]"
 override_attributes({
   "company" => "Daida",
 })
